9e9df194e27821c4fd01906fef2c700a65f04e24
brayanarroyochavez <brayan.arroyo.chavez@gmail.com>
2021-05-10 12:53:04 -0500

    cambio en la versión del chromedriver

--- wsBrandiumDJ/static/scraping/main.py
+++ wsBrandiumDJ/static/scraping/main.py
@@ -24,16 +24,15 @@ def scraping(request):
     #Variable p utilizada como contador con el propositos de pruebas.
     p = 1
     #Se asigna el interfaz de chrome.
-    #driver = webdriver.Chrome(ChromeDriverManager().install())
-    driver = webdriver.Chrome('wsBrandiumDJ/static/scraping/chromedriver.exe')
-    #Se abre la página principal del sitio web.
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-    #driver.get('https://www.tmdn.org/tmview/#/tmview')
+    #driver = webdriver.Chrome('wsBrandiumDJ/static/scraping/chromedriver.exe')
+    #Se abre la página principal del sitio web.
+    driver.get('https://www.tmdn.org/tmview/#/tmview')
     sleep(30)
     #find_element_by_xpath permite buscar etiquetas con atributos en especificos (Arroja el primer elemento encontrado)
     #el atributo debe de estar dentre de corchetes [] y el contenido se debe concatenar con un @ al inicio.
