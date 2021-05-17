ca927fa295e033c480a1b44de6aaaea9a1cf5d2c
brayanarroyochavez <brayan.arroyo.chavez@gmail.com>
2021-05-17 12:03:59 -0500

    Se agrego una ventana que indica el número de registros sin completar y se aplicaron validaciones para cuando existen registros completos

--- wsBrandiumDJ/static/scraping/connection.py
+++ wsBrandiumDJ/static/scraping/connection.py
@@ -3,8 +3,8 @@ import mysql.connector
 
 #Archivo de conexión a la base de datos
 mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="",
-  database="kampsys"
+  host="b5ieyox0oo6iha0jf6t8-mysql.services.clever-cloud.com",
+  user="u0u3tbtirq65ttqn",
+  password="gVp3XMYOqwj6a6kvOpN5",
+  database="b5ieyox0oo6iha0jf6t8"
 )
